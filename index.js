c225718d400578a1301dcd5fee681249ca23b48a
Furtuna-Mariana <furtuna.mariana@gmail.com>
2023-10-10 22:42:31 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// const arr = [1,2,3,4,5,6,7,8,9,10]
+
+
+// for (let i = 0; i <= 10; i++){
+//     debugger
+//     console.log(i)
+// }
+
+
+
+// const arr = [1,2,3,4,5,6,7,8,9,10]
+
+// let i = 0;
+// while (i<=10) {
+//     console.log(i);
+//     i++
+// }
+
+
+// const arr = [1,2,3,4,5,6,7,8,9,10]
+
+// let i = 0;
+
+// do {
+//     console.log(i)
+//     i++
+// } while (i<=10);
+
+// const arr = [1,2,3,4,5]
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// const arr = [1,2,3,4,5]
+
+// for (let i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+
+const arr = "Tema";
+
+for (let i = 0; i < arr.length; i++){
+    console.log(arr[i])
+}
---
+++ metodeleArray.js
@@ -0,0 +1,131 @@
+// const arr = [1,2,3,4,5]
+
+// console.log(arr.map((item, index, array) => {
+//     console.log('element', item)
+//     console.log('index', index)
+//     console.log('array', array)
+
+// }))
+
+
+
+
+// const arr = [1,2,3,4,5]
+
+// const arr1 = arr.filter((item, index) => {
+//     return index > 3
+// })
+
+// arr1.push(7)
+
+// console.log('arr', arr)
+// console.log('arr1', arr1)
+
+
+
+// const arr = [1,2,3,4,5]
+
+// const arr1 = arr.filter((item, index) => {
+//     return index > 3
+// })
+
+// arr1.push(7)
+
+// console.log('arr', arr)
+// console.log('arr1', arr1)
+
+
+
+// const arr = [1,2,3,4,5]
+
+// const idx = arr.indexOf(2)
+    
+// console.log('arr', arr)
+// console.log('arr1', idx)
+
+
+
+// const arr = [1,2,3,4,5]
+
+// const idx = arr.findIndex((item) => {
+//     return item > 2
+// })
+    
+// console.log('arr', arr)
+// console.log('arr1', idx)
+
+
+// const arr = [1,2,3,4,5]
+
+// const every = arr.every((item) => {
+//     return item > 0
+// })
+    
+// console.log('arr', arr)
+// console.log('arr1', every)
+
+
+
+
+// const arr = [1,2,3,4,5]
+
+// const some = arr.some((item) => {
+//     console.log(item)
+//     return item > 1
+// })
+    
+// console.log('arr', arr)
+// console.log('arr1', some)
+
+
+
+
+// const arr = [[1,2],3,[4,[5]]]
+
+
+// console.log('arr', arr)
+// console.log('flat', arr.flat(2))
+
+
+
+
+// const arr = [[1,2],3,[4,[5]]]
+
+// arr.forEach((item, index) => {
+// console.log('item', item)
+// console.log('index', index)
+// })
+
+// 
+
+
+
+
+// const arr = [1,2,3,4,5]
+
+// const found = arr.find((item) => {
+//     return item > 4
+// })
+
+// console.log('arr', arr)
+// console.log('found', found)
+
+
+
+
+// const arr = [1,2,3,4,5]
+// const arr1 = [6,7,8,9,10]
+// const arr2 = [...arr, ...arr1]
+
+
+
+// console.log('arr', arr)
+// console.log('arr1', arr1)
+// console.log('arr2', arr2)
+
+
+
+const arr = [1,2,3,4,5]
+
+console.log('arr', arr)
+console.log('includes', arr.includes(6))
